03d5015e90645f0e1395e471a67ed208fe199939
Pavel Bodrug <plkoij73@gmail.com>
2025-07-06 15:37:21 +0300

    added Lock

--- wallet-api/src/main/java/com/example/wallet_api/repository/WalletJpa.java
+++ wallet-api/src/main/java/com/example/wallet_api/repository/WalletJpa.java
@@ -1,10 +1,15 @@
 package com.example.wallet_api.repository;
 
 import com.example.wallet_api.entity.Wallet;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletJpa extends JpaRepository<Wallet, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    Optional<Wallet> findById(UUID uuid);
 }
